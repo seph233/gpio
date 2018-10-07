c7286a4319bc2fb7ef5610667baab4db39c577f3
Seph <seph233@gmail.com>
2018-10-07 07:03:50 +0000

    commit

---
+++ gpio.py
@@ -0,0 +1,34 @@
+from flask import Flask
+import RPi.GPIO as GPIO #import the GPIO library
+import datetime
+import time
+
+app = Flask(__name__)
+
+def logmsg(message):
+    print(message)
+    with open("logfile", "a") as f:
+        f.write(str(datetime.datetime.now()) + " - " + message)
+
+@app.route("/getgpiostatus/<pin>")
+def getdevicepowerstatus(pin):
+    response = GPIO.input(int(pin))
+
+    if response:
+      result = "1"
+    elif response == False:
+      result = "0"
+    else:
+      result = "unknown"
+
+    #logmsg("gpio status for pin " + pin + ": " + result + "\n")
+    return result
+
+def setup_app():
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+setup_app()
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=12002)
